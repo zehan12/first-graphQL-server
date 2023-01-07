28fd6deb5795b684a5d563501a36121b017df3b3
Zehan Khan <zehan9211@gmail.com>
2023-01-07 17:33:30 +0530

    install graphql & express-graphql and import it

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require("express");
-const app = express()
+const app = express();
+const PORT = 6969;
+const userData = require("./Faker.json");
+const graphql = require("graphql")
+const { graphqlHTTP } = require("express-graphql") 
+
+app.listen(PORT,(err)=>{
+    console.log(err ? "Server Crashed" :"Server is serving faster than a supercar")
+})
